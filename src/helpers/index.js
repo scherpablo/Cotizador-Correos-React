7d36b1f06ded226e78ed360752377ae108c4b33b
scherpablo <scherpablo@gmail.com>
2023-06-02 18:43:52 -0300

    update index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,28 +7,6 @@ const formatearDinero = (valor) => {
     return formatter.replace(/,/g, ".");
 };
 
-// const devolverDinero = (correos, plazo) => {
-//     let total;
-//     const valor = 1299;
-//     const valor12 = 8388;
-//     const valor24 = 14376;
-//     const valor48 = 23952;
-
-//     if (correos) {
-//         total = correos;
-//     }
-
-//     if (plazo === 12) {
-//         total *= valor12 * 1.5;
-//     }else if (plazo === 24) {
-//         total *= valor24 * 1.5;
-//     }else if (plazo === 48) {
-//         total *= valor48 * 1.5;
-//     }else {
-//         total *= valor * 1.5;    
-//     }
-//     return total;
-// }
 const devolverDinero = (correos, plazo) => {
     let total = 0;
 
@@ -52,26 +30,6 @@ const devolverDinero = (correos, plazo) => {
 const calcularPagoMensual = (total, plazo) => {
     return total / plazo;
 }
-
-function textoPluralSingular(valor, textoSingular, textoPlural) {
-    if (valor === 1) {
-        return textoSingular;
-    }
-    return textoPlural;
-}
-
-function textoCorreos(correos) {
-    return textoPluralSingular(correos, "correo", "correos");
-}
-
-function textoMeses(meses) {
-    return textoPluralSingular(meses, "Mes", "Meses");
-}
-
-function textoCuotas(meses) {
-    return textoPluralSingular(meses, "cuota", "cuotas");
-}
-
 
 export {
     formatearDinero,
