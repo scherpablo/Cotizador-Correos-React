2c86af1492bc83af006f461835fbfe50ce1fef59
scherpablo <scherpablo@gmail.com>
2023-06-06 01:28:01 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -117,7 +117,7 @@ function App() {
           <span className="text-indigo-600 font-extrabold"> {span} </span>
           <p className="text-xl text-center font-extrabold mt-5">* Período <span className="text-indigo-600 font-extrabold">{meses} {textoMeses()}</span> *</p>
           <p className="text-xl text-center font-extrabold mt-2">* 1 pago de <span className="text-green-600 font-extrabold">{formatearDinero(total)}</span> *</p>
-          <p className="text-xl text-center font-extrabold mt-2">* {meses} {textoCuotas()} de <span className="text-indigo-600 font-extrabold">{formatearDinero(pago)}</span> *</p>
+          {/* <p className="text-xl text-center font-extrabold mt-2">* {meses} {textoCuotas()} de <span className="text-indigo-600 font-extrabold">{formatearDinero(pago)}</span> *</p> */}
         </h2>
         <div className="text-center">
           <a href="https://www.mercadopago.com.ar" target="_blank" rel="noopener noreferrer">
